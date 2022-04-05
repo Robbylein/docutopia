74c4fc19c3be2f090e4d3f7dfba772c2bfd4e7bd
AT <mail@antontranelis.de>
2022-04-05 21:32:57 +0200

    useState for places and events

--- src/Pages/Map.js
+++ src/Pages/Map.js
@@ -4,83 +4,64 @@ import 'react-leaflet-markercluster/dist/styles.min.css'; // sass
 import { MapContainer, TileLayer,  LayersControl, Marker } from 'react-leaflet'
 import MarkerPopup from '../Components/Map/MarkerPopup'
 import { Directus } from '@directus/sdk';
-import { Async } from "react-async";
 import L from 'leaflet';
 import MarkerClusterGroup from 'react-leaflet-markercluster';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import SpeedDialButton from '../Components/Map/SpeedDialButton';
 import MarkerIconFactory from '../Utils/MarkerIconFactory';
 
 const directus = new Directus('https://zeitgeist.healing-the-planet.one');
 
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-});
-
-const loadPlaces = () => directus.items('Places').readByQuery({fields: '*.Tags_id.color,*.Tags_id.id'  });
-const loadEvents = () => directus.items('Events').readByQuery({fields: '*.Tags_id.color,*.Tags_id.id' });
-
-
 const Map = () => {
+  const [places, setPlaces] = useState(null);
+  const [events, setEvents] = useState(null);
   useEffect(() => {
     setMapHeight()
-  })
+    directus.items('Places').readByQuery({fields: '*.Tags_id.color,*.Tags_id.id'  })
+      .then(res => {
+        setPlaces(res.data);
+      });
+    directus.items('Events').readByQuery({fields: '*.Tags_id.color,*.Tags_id.id' })
+      .then(res => {
+        setEvents(res.data);
+      });
+  },[])
 
   return (
     <div id="map" className="Leaflet">
       <MapContainer style={{ height: "100vh",   width: "100vw" }} center={[51.3, 9.6]} zoom={8}>
         <LayersControl position="topright">
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <LayersControl.Overlay checked name="Places">
-        <MarkerClusterGroup>
-          <Async promiseFn={loadPlaces}>
-           {({ data, error, isPending }) => {
-            if (isPending) console.log("Loading Places ...");
-            if (error) console.log(`Something went wrong: ${error.message}`);
-            if (data) {
-            console.log(data);
-              return (
-                (data.data).map((place) => (
-                  <Marker icon={MarkerIconFactory('penta','#f18e1c','RGBA(35, 31, 32, 0.2)','circle-solid')} key={place.id} position={[place.position.coordinates[1],place.position.coordinates[0]]}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <LayersControl.Overlay checked name="Places">
+            <MarkerClusterGroup>
+              {places &&
+                (places).map((place) => (
+                  <Marker icon={MarkerIconFactory('circle','#f18e1c','RGBA(35, 31, 32, 0.2)','circle-solid')} key={place.id} position={[place.position.coordinates[1],place.position.coordinates[0]]}>
                     <MarkerPopup item={place}>
                     </MarkerPopup>
                   </Marker>
                 ))
-              )}
-          }}
-        </Async>
-      </MarkerClusterGroup>
-      </LayersControl.Overlay>
-      <LayersControl.Overlay checked name="Events">
-      <MarkerClusterGroup>
-        <Async promiseFn={loadEvents}>
-         {({ data, error, isPending }) => {
-          if (isPending) console.log("Loading Events ...");
-          if (error) console.log(`Something went wrong: ${error.message}`);
-          if (data)
-            return (
-              (data.data).map((event) => (
-                <Marker icon={MarkerIconFactory('square','#6d398b','RGBA(35, 31, 32, 0.2)','calendar-days-solid')} key={event.id} position={[event.position.coordinates[1],event.position.coordinates[0]]}>
-                  <MarkerPopup item={event}>
-                  </MarkerPopup>
-                </Marker>
-              ))
-            )
-        }}
-      </Async>
-    </MarkerClusterGroup>
-    </LayersControl.Overlay>
-      </LayersControl>
+              }
+            </MarkerClusterGroup>
+          </LayersControl.Overlay>
+          <LayersControl.Overlay checked name="Events">
+            <MarkerClusterGroup>
+              {events &&
+                (events).map((event) => (
+                  <Marker icon={MarkerIconFactory('square','#6d398b','RGBA(35, 31, 32, 0.2)','calendar-days-solid')} key={event.id} position={[event.position.coordinates[1],event.position.coordinates[0]]}>
+                    <MarkerPopup item={event}>
+                    </MarkerPopup>
+                  </Marker>
+                ))
+              }
+            </MarkerClusterGroup>
+          </LayersControl.Overlay>
+        </LayersControl>
       </MapContainer>
       <SpeedDialButton />
-
     </div>
   )
 }
